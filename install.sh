842ffdd0b382fcebb3cf613d646392d786d9720b
nvitius <mauricio@nviti.us>
2018-03-29 11:29:47 -0700

    Use pgp extension instead of gpg

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ sudo chsh -s /usr/local/bin/zsh `whoami`
 
 keybase login nvitius
 
-(cd $HOME/.dotfiles/fonts && keybase pgp decrypt -i op_mono.tar.gz.gpg | tar -zxv)
-(cd $HOME/.dotfiles/licenses && keybase pgp decrypt -i SizeUp.sizeuplicense.gpg -o SizeUp.sizeuplicense)
-(cd $HOME/.dotfiles && keybase pgp decrypt -i ssh.tar.gz.gpg | tar -zxv)
+(cd $HOME/.dotfiles/fonts && keybase pgp decrypt -i op_mono.tar.gz.pgp | tar -zxv)
+(cd $HOME/.dotfiles/licenses && keybase pgp decrypt -i SizeUp.sizeuplicense.pgp -o SizeUp.sizeuplicense)
+(cd $HOME/.dotfiles && keybase pgp decrypt -i ssh.tar.gz.pgp | tar -zxv)
 (cd $HOME/.dotfiles/kube && keybase pgp decrypt -i config.pgp -o config)
 
 ln -s $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
